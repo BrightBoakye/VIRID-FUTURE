a2315340a2ef6d95dc7ca71336faebe8db152b9d
lorde <47636645+lordofdeceit@users.noreply.github.com>
2023-05-20 13:13:56 +0300

    Update ViridAI_optimized.py
    added documentation for relation. most changes asme as the google bard as this one is based of it.

--- ViridAI_optimized.py
+++ ViridAI_optimized.py
@@ -7,8 +7,17 @@ import config
 # Set up OpenAI API credentials
 openai.api_key = "[SECRET-API-KEY]"
 
-# function to call OpenAI API and get insights and recommendations
-def get_insights(land_type):
+def get_insights_and_recommendations(land_type):
+    """
+    Get insights and recommendations for a given land type.
+
+    Args:
+        land_type (str): The land type to get insights and recommendations for.
+
+    Returns:
+        str: The insights and recommendations for the given land type.
+    """
+
     prompt = f"What are the best practices for optimizing yield in {land_type} land type ?"
     response = openai.Completion.create(
         engine="text-davinci-003",
@@ -18,36 +27,39 @@ def get_insights(land_type):
         stop=None,
         temperature=0.8,
     )
-    return response.choices[0].text.strip()
-
-
-def decode_target(target, text_labels=False):
-    """Decode target labels into text (or not)"""
-    if text_labels:
-        return config.IDX_CLASS_LABELS[target]
-    else:
-        return target
+    recommendation = response.choices[0].text.strip()
+    return recommendation
 
 
 def predict_single(image):
+    """
+    Predict the land type of an image.
+
+    Args:
+        image (torch.Tensor): The image to predict the land type of.
+
+    Returns:
+        tuple(str, str): The predicted land type and insights and recommendations for the predicted land type.
+    """
+
     device = get_device()
     xb = image.unsqueeze(0)
     xb = to_device(xb, device)
     with torch.no_grad():
         preds = model(xb)
     _, prediction = torch.max(preds.cpu().detach(), dim=1)
-    
+
     # Get insights and recommendations using OpenAI API
     land_type = decode_target(int(prediction), text_labels=True)
-    insights = get_insights(land_type)
-    
-    return land_type, insights
+    insights_and_recommendations = get_insights_and_recommendations(land_type)
+
+    return land_type, insights_and_recommendations
 
 
 if __name__ == "__main__":
     image = get_image(config.PATH)
-    land_type, insights = predict_single(image)
+    land_type, insights_and_recommendations = predict_single(image)
     print(f"Predicted land type: {land_type}")
-    print(f"Insights and recommendations: {insights}")
+    print(f"Insights and recommendations: {insights_and_recommendations}")
 else:
     print("Sorry no insights for this landtype")
